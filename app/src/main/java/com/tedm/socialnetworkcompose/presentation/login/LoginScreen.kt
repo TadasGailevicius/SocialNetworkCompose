27b4a7a4ef5c305a095ffb3e121f0f59535f9f5b
Tadas <tadasgailevicius@yahoo.com>
2021-08-02 21:25:30 +0300

    Add LoginScreen.kt Screen

---
+++ app/src/main/java/com/tedm/socialnetworkcompose/presentation/login/LoginScreen.kt
@@ -0,0 +1,22 @@
+package com.tedm.socialnetworkcompose.presentation.login
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavController
+
+@Composable
+fun LoginScreen(navController: NavController) {
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "This is Login Screen", color = Color.White)
+    }
+    
+}
