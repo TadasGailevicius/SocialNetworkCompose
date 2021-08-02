50faee416f73684dde1865b3ab2ba416372e98d3
Tadas <tadasgailevicius@yahoo.com>
2021-08-02 21:22:00 +0300

    Move ui package to presentation package

--- app/src/main/java/com/tedm/socialnetworkcompose/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/tedm/socialnetworkcompose/presentation/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.tedm.socialnetworkcompose.ui.theme
+package com.tedm.socialnetworkcompose.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
--- app/src/main/java/com/tedm/socialnetworkcompose/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/tedm/socialnetworkcompose/presentation/ui/theme/Theme.kt
@@ -1,9 +1,7 @@
-package com.tedm.socialnetworkcompose.ui.theme
+package com.tedm.socialnetworkcompose.presentation.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
