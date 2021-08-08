48beaea34ef13907829bf1a345f293a28f55a41a
Tadas <tadasgailevicius@yahoo.com>
2021-08-09 01:02:21 +0300

    Fix Android Lint Warnings

--- app/src/main/java/com/tedm/socialnetworkcompose/presentation/util/Screen.kt
+++ app/src/main/java/com/tedm/socialnetworkcompose/presentation/util/Screen.kt
@@ -1,6 +1,6 @@
 package com.tedm.socialnetworkcompose.presentation.util
 
-sealed class Screen(val route: String, ) {
+sealed class Screen(val route: String) {
     object SplashScreen : Screen("splash_screen")
     object LoginScreen : Screen("login_screen")
     object RegisterScreen : Screen("register_screen")
