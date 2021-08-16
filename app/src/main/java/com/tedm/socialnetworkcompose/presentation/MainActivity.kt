0e696d8949b01bc7359492f67259f276082821c7
Tadas <tadasgailevicius@yahoo.com>
2021-08-16 16:01:29 +0300

    Implement Toolbar

--- app/src/main/java/com/tedm/socialnetworkcompose/presentation/MainActivity.kt
+++ app/src/main/java/com/tedm/socialnetworkcompose/presentation/MainActivity.kt
@@ -36,6 +36,14 @@ class MainActivity : ComponentActivity() {
                             Screen.ActivityScreen.route,
                             Screen.ProfileScreen.route,
                         ),
+                        showBackArrow = navBackStackEntry?.destination?.route in listOf(
+                            Screen.PostDetailScreen.route,
+                            Screen.MessagesScreen.route,
+                            Screen.EditProfileScreen.route,
+                            Screen.SearchScreen.route,
+                            Screen.CreatePostScreen.route,
+                            Screen.PersonListScreen.route,
+                        ),
                         modifier = Modifier.fillMaxSize(),
                         onFabClick = {
                             navController.navigate(Screen.CreatePostScreen.route)
--- app/src/main/java/com/tedm/socialnetworkcompose/presentation/main_feed/MainFeedScreen.kt
+++ app/src/main/java/com/tedm/socialnetworkcompose/presentation/main_feed/MainFeedScreen.kt
@@ -1,10 +1,22 @@
 package com.tedm.socialnetworkcompose.presentation.main_feed
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.navigation.NavController
+import com.tedm.socialnetworkcompose.R
 import com.tedm.socialnetworkcompose.presentation.components.Post
+import com.tedm.socialnetworkcompose.presentation.components.StandardToolbar
+import com.tedm.socialnetworkcompose.presentation.util.Screen
 
 @Composable
 fun MainFeedScreen(
@@ -13,13 +25,38 @@ fun MainFeedScreen(
     Column(
         modifier = Modifier.fillMaxWidth()
     ) {
+        StandardToolbar(
+            navController = navController,
+            title = {
+                Text(
+                    text = stringResource(id = R.string.your_feed),
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colors.onBackground
+                )
+            },
+            modifier = Modifier.fillMaxWidth(),
+            showBackArrow = true,
+            navActions = {
+                IconButton(onClick = {
+                    navController.navigate(Screen.SearchScreen.route)
+                }) {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = "",
+                        tint = MaterialTheme.colors.onBackground,
+                    )
+                }
+            }
+
+        )
+        
         Post(
             post = com.tedm.socialnetworkcompose.domain.models.Post(
-                username = "Philipp Lackner",
+                username = "Tadas Gailevičius",
                 imageUrl = "",
                 profilePictureUrl = "",
-                description = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed\n" +
-                        "diam nonumy eirmod tempor invidunt ut labore et dolore \n" +
+                description = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed " +
+                        "diam nonumy eirmod tempor invidunt ut labore et dolore " +
                         "magna aliquyam erat, sed diam voluptua...",
                 likeCount = 17,
                 commentCount = 7
