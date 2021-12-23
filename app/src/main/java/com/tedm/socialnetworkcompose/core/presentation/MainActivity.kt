fe560bfa55e8f3338d768c916093dfa0338f0b5e
Tadas <tadasgailevicius@yahoo.com>
2021-12-24 01:32:51 +0200

    Update build.gradle dependencies

--- app/src/main/java/com/tedm/socialnetworkcompose/core/presentation/MainActivity.kt
+++ app/src/main/java/com/tedm/socialnetworkcompose/core/presentation/MainActivity.kt
@@ -20,7 +20,7 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @ExperimentalMaterialApi
+    @OptIn(ExperimentalMaterialApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
